54d9655a324460c6dd449d2168516c23ae4c930b
unknown <Jonipux@yandex.ru>
2023-05-03 03:13:58 +0300

    добавил компонент поиска по фильмам. поправил хедер.

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from '../Header/Header';
+import SearchForm from "../SearchForm/SearchForm";
 
 export default function Movies() {
   return (
     <>
     <Header movies={true} />
     <main className="content">
-      
+      <SearchForm />
     </main>
     </>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,29 @@
 import React from "react";
 import './SearchForm.scss';
+import { useState } from "react";
 
 export default function SearchForm() {
+  const [checked, setChecked] = useState(true);
+
+  function setCheckbox() {
+    setChecked(!checked);
+  }
+  
   return (
-    <section className="search">
+    <section className="search indent-section">
       <form className="search__form">
-        <input type="text" placeholder="Фильм" className="search__field" name="search" required />
-        <button type="submit" className="search__submit"></button>
+        <div className="search__container-field">
+          <input type="text" placeholder="Фильм" className="search__field" name="search" required />
+          <button type="submit" className="search__submit"></button>
+        </div>
+        <div className="search__container-checkbox">
+          <input type="checkbox" className="search__checkbox" name="short" id="short"
+          checked={checked ? true : false}
+          onClick={setCheckbox}
+          />
+          <label for="short" className="search__checkbox-custom"></label>
+          <label for="short" className="search__short-films">Короткометражки</label>
+        </div>
       </form>
     </section>
   );
